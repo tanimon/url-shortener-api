d10cc3aac0231ea163c6030c901064177dad5951
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-01 14:07:12 +0900

    Reformat

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
