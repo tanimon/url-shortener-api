9406c1cc0f24c1851e8083fdd02ab6a489bedfa1
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-01 14:09:50 +0900

    Remove an unnecessary endpoint

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
