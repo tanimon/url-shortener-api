5195bde612883270131049bf60cf8d2b37ff6b7a
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-04 18:58:50 +0900

    Add urlencoded middleware

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
